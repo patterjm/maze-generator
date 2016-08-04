d2ec027affcf5fc300f8588206aa446330e4a3bf
Jacob Patterson <Jacob Patterson>
2016-08-04 17:58:24 -0400

    Add new dir

---
+++ MazeGenerator/MazeCell.py
@@ -0,0 +1,47 @@
+"""
+This class is meant to be the singular "spot" in any portion of the maze.
+It defines itself by 5 booleans (encoded as integers by Python, probably, not sure)
+4 of which are the walls where 1 - wall exists eg MazeCell.north = 1 signifies the north wall
+is in place.
+
+The leftover boolean dictates whether a cell has been visited. Primarily used
+in recursive backtracking algorithm, will be removed upon further iterations, should this be
+improved, as the class should be independent of the algorithm using it
+"""
+
+
+class MazeCell:
+    # simple 5 param constructor
+    def __init__(self, north, east, south, west, visited):
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+        self.visited = visited
+
+    # Python's way of overriding the toString method
+    def __str__(self):
+        retstr = "North " + str(self.north)
+        retstr += " South " + str(self.south)
+        retstr += " East " + str(self.east)
+        retstr += " West " + str(self.west)
+        retstr += " Visited " + str(self.visited)
+        return retstr
+
+    # Marks the cell as visited
+    def visit(self):
+        self.visited = 1
+
+    # Destroys (sets to 0) any walls given a cardinal direction
+    # @param: direction
+    def breaker(self, direction):
+        if direction == 'north':
+            self.north = 0
+        elif direction == 'south':
+            self.south = 0
+        elif direction == 'west':
+            self.west = 0
+        elif direction == 'east':
+            self.east = 0
+        else:
+            print("Something went wrong when breaking a cell's wall!")
---
+++ MazeGenerator/MazeGrid.py
@@ -0,0 +1,37 @@
+"""
+This class is the overarching m x n grid that houses cells
+
+More specifically, it can be used to providing bounding figures to our
+xy plane problem space
+"""
+from MazeCell import *
+
+
+class MazeGrid:
+    # constructor
+    # @params: rows - number of rows to instantiate
+    # cols - number of columns to instantiate
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        # this is a pythonic way of creating a 2D array
+        # We essentially create a list of length cols for each element in a list of length rows
+        self.gridSpace = [[0 for x in range(self.cols)] for y in range(self.rows)]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                x = MazeCell(1, 1, 1, 1, 0)
+                self.gridSpace[i][j] = x
+
+    # Will print 'Cell at space i x j str(MazeCell[i][j]) \n' for each cell in the grid
+    # where i, j represented the current row and column offsets respectively
+    # This is stored in ROW MAJOR order
+    # eg consider a 2x3 maze, the 1st row will be:
+    # MazeGrid[0][0] , MazeGrid[0][1] , MazeGrid[0][2]
+    def __str__(self):
+        retstr = ""
+        for i in range(self.rows):
+            for j in range(self.cols):
+                retstr += "Cell at space " + str(i) + " x " + str(j)
+                retstr += " " + str(self.gridSpace[i][j]) + "\n"
+
+        return retstr
